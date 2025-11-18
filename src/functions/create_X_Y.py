82578d90632d4dd7c131b65da8fa6bde8afa729b
leoQUENETTE <leoettag@gmail.com>
2025-11-18 14:59:21 +0100

    Update createX_Y function

--- src/functions/create_X_Y.py
+++ src/functions/create_X_Y.py
@@ -3,7 +3,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 
-def create_x_y(sequence_length : int, df : list[str]):
+def create_x_y(sequence_length : int, df : any):
     tokenizer = Tokenizer(num_words=sequence_length, oov_token="<OOV>") # Tester avec sequence_length = 36 ? (pas satisfaite des datas du prof)
     tokenizer.fit_on_texts(df['caption']) # Apprentissage du vocabulaire
     sequences = tokenizer.texts_to_sequences(df['caption'])
