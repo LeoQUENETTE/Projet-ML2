00b8875821fddc057590aa493e42882e72bb1dec
leoQUENETTE <leoettag@gmail.com>
2025-11-18 15:34:32 +0100

    ok

--- src/functions/create_X_Y.py
+++ src/functions/create_X_Y.py
@@ -4,10 +4,9 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 
 
 def create_x_y(sequence_length : int, df : any):
-    print("test create X Y")
-    # tokenizer = Tokenizer(num_words=sequence_length, oov_token="<OOV>") # Tester avec sequence_length = 36 ? (pas satisfaite des datas du prof)
-    # tokenizer.fit_on_texts(df['caption']) # Apprentissage du vocabulaire
-    # sequences = tokenizer.texts_to_sequences(df['caption'])
-    # max_len = max(len(s) for s in sequences)
-    # X = pad_sequences(sequences, maxlen=max_len, padding="post")
-    # y = np.array(df['label'])
+    tokenizer = Tokenizer(num_words=sequence_length, oov_token="<OOV>") # Tester avec sequence_length = 36 ? (pas satisfaite des datas du prof)
+    tokenizer.fit_on_texts(df['caption']) # Apprentissage du vocabulaire
+    sequences = tokenizer.texts_to_sequences(df['caption'])
+    max_len = max(len(s) for s in sequences)
+    X = pad_sequences(sequences, maxlen=max_len, padding="post")
+    y = np.array(df['label'])
