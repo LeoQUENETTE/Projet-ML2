ed60c89b1dc9e65563f73d60228e610477ef7ae9
leoQUENETTE <leoettag@gmail.com>
2025-11-18 14:23:00 +0100

    Added a damn __init__ file inside functions

--- src/functions/create_X_Y.py
+++ src/functions/create_X_Y.py
@@ -3,10 +3,10 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 
-def create_x_y(sequence_length : int, max_len : int, df : list[str]):
+def create_x_y(sequence_length : int, df : list[str]):
     tokenizer = Tokenizer(num_words=sequence_length, oov_token="<OOV>") # Tester avec sequence_length = 36 ? (pas satisfaite des datas du prof)
     tokenizer.fit_on_texts(df['caption']) # Apprentissage du vocabulaire
     sequences = tokenizer.texts_to_sequences(df['caption'])
-
+    max_len = max(len(s) for s in sequences)
     X = pad_sequences(sequences, maxlen=max_len, padding="post")
     y = np.array(df['label'])
