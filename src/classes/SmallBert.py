f6c6c3288b99ebd24809ee3c5c612fb419052cf5
leoQUENETTE <leoettag@gmail.com>
2025-12-05 12:04:45 +0100

    test import

--- src/classes/SmallBert.py
+++ src/classes/SmallBert.py
@@ -1,4 +1,4 @@
-from UsefullClasses import PositionalEmbedding
+from src.classes.UsefullClasses import PositionalEmbedding
 from TransformerBlock import TransformerBlock
 from tensorflow.keras import layers
 from tensorflow.keras.utils import register_keras_serializable
